c499c8bc98703b87319ad11038a7ffdcaef280aa
TL <TL@gmail.com>
2023-01-29 11:38:41 +0530

    dev1 modified test1.java

--- DemoProj1/src/com/nt/test1/Hello.java
+++ DemoProj1/src/com/nt/test1/Hello.java
@@ -1,5 +1,11 @@
 package com.nt.test1;
 
+import com.nt.beans.Utility;
+
 public class Hello {
+	public static void main(String[] args) {
+		Utility u=new Utility();
+		System.out.println(u.getByDate());
+	}
 
 }
